b3dcfdb7c495c5d7cc3b585b86db020f1bfae3f6
kareem elshaar <kemo.elshaar22@gmail.com>
2024-06-12 20:49:47 +0300

    shared preference  --> kareem elshaar 12/6

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -1,3 +1,4 @@
+import 'package:dalel_app/core/database/cache/cahche_helper.dart';
 import 'package:dalel_app/core/utils/app_strings.dart';
 import 'package:dalel_app/core/utils/app_text_style.dart';
 import 'package:flutter/material.dart';
@@ -14,7 +15,13 @@ class SplashView extends StatefulWidget {
 class _SplashViewState extends State<SplashView> {
   @override
   void initState() {
-    delayedNavigate(context);
+    bool isOnBoardingIsVisited =
+        CacheHelper().getData(key: "isOnBoardingIsVisited") ?? false;
+    if (isOnBoardingIsVisited == true) {
+      delayedNavigate(context, "/signup");
+    } else {
+      delayedNavigate(context, "/onBoarding");
+    }
     super.initState();
   }
 
@@ -31,8 +38,8 @@ class _SplashViewState extends State<SplashView> {
   }
 }
 
-void delayedNavigate(context) {
+void delayedNavigate(context, String path) {
   Future.delayed(const Duration(seconds: 2), () {
-    customReplacementNavigate(context, "/onBoarding");
+    customReplacementNavigate(context, path);
   });
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:dalel_app/core/database/cache/cahche_helper.dart';
 import 'package:dalel_app/core/utils/app_colors.dart';
 import 'package:flutter/material.dart';
 
 import 'core/routes/app_router.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await CacheHelper().init();
   runApp(const Dalel());
 }
 

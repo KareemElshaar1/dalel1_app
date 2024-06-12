fa88ab48e6907e3ea549dae2ed1a6d68f0b7a6a9
kareem elshaar <kemo.elshaar22@gmail.com>
2024-06-12 03:12:44 +0300

    app_router.dart.dart --> kareem elshaar 12/6  add go router and refactor it

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'core/routes/app_router.dart';
+
 void main() {
   runApp(const Dalel());
 }
@@ -9,6 +11,8 @@ class Dalel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp.router(
+      routerConfig: router,
+    );
   }
 }
